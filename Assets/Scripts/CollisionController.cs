39fa8a1b94efff7539601526956aa6f961b067da
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-10-26 21:31:48 -0500

    feat: package destroy

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CollisionController : MonoBehaviour
 {
     bool hasPackage = false;
+
+    [SerializeField]
+    private float destroyDelay = 0.2f;
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -12,10 +15,11 @@ public class CollisionController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if ( other.tag == "Package" )
+        if ( other.tag == "Package" && !hasPackage )
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
 
         if ( other.tag == "Customer" && hasPackage )
