1ece165f76151a8503aa3c0570aa11b456341706
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-10-26 21:45:01 -0500

    feat: change player color

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -8,6 +8,21 @@ public class CollisionController : MonoBehaviour
 
     [SerializeField]
     private float destroyDelay = 0.2f;
+
+    [SerializeField]
+    private Color32 packageColor = new Color32 (1,1,1,1);
+
+    
+    [SerializeField]
+    private Color32 originalColor = new Color32 (1,1,1,1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -20,12 +35,14 @@ public class CollisionController : MonoBehaviour
             Debug.Log("Package picked up");
             hasPackage = true;
             Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = packageColor;
         }
 
         if ( other.tag == "Customer" && hasPackage )
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = originalColor;
         }
     }
 }
