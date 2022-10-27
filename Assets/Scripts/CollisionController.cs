de36fc26c5c812dc60a704c4be115339fd7dc307
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-10-26 21:20:42 -0500

    feat: package delivery logic

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CollisionController : MonoBehaviour
 {
+    bool hasPackage = false;
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -11,6 +12,16 @@ public class CollisionController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("What was that?");
+        if ( other.tag == "Package" )
+        {
+            Debug.Log("Package picked up");
+            hasPackage = true;
+        }
+
+        if ( other.tag == "Customer" && hasPackage )
+        {
+            Debug.Log("Package delivered");
+            hasPackage = false;
+        }
     }
 }
